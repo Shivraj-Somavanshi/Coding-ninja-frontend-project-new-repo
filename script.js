a963edfc7dcc52a54e48b48547a88d0af70039b2
Shivraj-Somavanshi <shivasomavanshi@gmail.com>
2024-02-29 15:21:30 +0530

    this is first time commit of the project to do list

---
+++ script.js
@@ -0,0 +1,36 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+const addbutton = document.getElementById("addbutton");
+const taskCountSpan = document.getElementById("task-count");
+
+let taskCount = 0;
+
+addbutton.addEventListener("click", () => {
+  if (inputBox.value === "") {
+    alert("You must write something");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+    taskCount++;
+    updateTaskCount();
+  }
+  inputBox.value = "";
+});
+
+listContainer.addEventListener("click", (e) => {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+  } else if (e.target.tagName === "SPAN") {
+    e.target.parentElement.remove();
+    taskCount--;
+    updateTaskCount();
+  }
+});
+
+function updateTaskCount() {
+  taskCountSpan.textContent = taskCount;
+}
